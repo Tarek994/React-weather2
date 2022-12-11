b6195a0c2e169045583465094aa6fcaaec3ffffc
Ali Ghali <ali.ghali@zalando.de>
2022-12-12 00:47:36 +0100

    fix(app): fetch city weather data via https

--- src/App.js
+++ src/App.js
@@ -1,50 +1,47 @@
-import React,{useState} from "react"
+import React, { useState } from "react"
 import './App.css';
 
 
 function App() {
-
   const apiKey = "6b42ac7df85a4e3c6a182a15d6784e03"
-  const {weatherData , setWeatherData} = useState([{}]);
-  const [city, setCity ] = useState("")
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [cityName, setCityName] = useState();
+  const [cityTemp, setCityTemp] = useState();
+  const [cityWeather, setCityWeather] = useState();
+  const [city, setCity] = useState("")
 
-  const getWeather = (event) => {
-    if(event.key === "Enter"){
-      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&units=metric&appid=${apiKey}`).then(
-        response => response.json()
-      ).then(
-        data => {
-          setWeatherData(data);
-          setCity("")
-        }
-      )
+  const getWeather = async (event) => {
+    if (event.key === "Enter") {
+      const fetchPromise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&units=metric&appid=${apiKey}`)
+      const cityWeatherData = await fetchPromise.json();
+      setCityName(cityWeatherData.name)
+      setCityTemp(cityWeatherData.main.temp)
+      setCityWeather(cityWeatherData?.weather[0].main)
+      setWeatherData(cityWeatherData)
+      setCity("")
     }
   }
 
   return (
     <div className="container">
       <input
-      className="input"
-      placeholder="Enter Cite.."
-      onChange={e => setCity(e.target.value)}
-      value={city}
-      onKeyPress={getWeather}
+        className="input"
+        placeholder="Enter City.."
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
       />
 
-{typeof weatherData?.main && 'undefined' ? (
-        <div>
+      {
+        weatherData?.main && <div>
           <p className="welcome">Welcome to weather app! Enter in a city to get the weather</p>
         </div>
-      ) : (
-        <div>
-          <p>{weatherData.name}</p>
-          <p>{Math.round(weatherData.temp)}˚F</p>
-          <p>{weatherData.weather[0].main}</p>
-
-        </div>
-      )}
-
-      
+      }
+      <div>
+        {cityName && <p>{cityName}</p>}
+        {cityTemp && <p>{Math.round(cityTemp)}˚F</p>}
+        {cityWeather && <p>{cityWeather}</p>}
+      </div>
     </div>
   );
 }
